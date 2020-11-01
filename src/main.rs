6e74202084631048f4fcb82ec818cbdd751e297a
Amir <amir.anwar.said@gmail.com>
2020-11-01 02:09:01 +0200

    initial cli args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// use sqlite::{Connection, Result};
+mod types;
+use std::env;
+use types::AddressRecord;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut name = String::from("");
+    let mut mobile = String::from("");
+    let mut address = String::from("");
+    let mut good_arguments = true;
+    for arg in &args[1..] {
+        if arg.starts_with("--name=") {
+            name = arg[7..].to_string();
+        } else if arg.starts_with("--mobile=") {
+            mobile = arg[10..].to_string();
+        } else if arg.starts_with("--address=") {
+            address = arg[10..].to_string();
+        } else{
+            println!("Bad Arguments!");
+            good_arguments = false;
+        }
+    }
+    if good_arguments{
+        let address_record =  AddressRecord::new(name, mobile, address);
+        println!("Name: {}", address_record.name);
+        println!("Mobile: {}", address_record.mobile);
+        println!("Address: {}", address_record.address);
+
+    }
 }
